db2b5a440a7e0499c67e1174134ee54ab0910a1b
Lovemysi <3236705586@qq.com>
2024-05-30 21:42:30 +0800

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import DashSidebar from "../conponents/DashSidebar";
+import DashProfile from "../conponents/DashProfile";
+import { useLocation } from "react-router-dom";
 
+// 仪表盘用于 操作文章,设置用户
+// 通过 url传递不同参数路由到不同仪表盘
+// 先挂载的后渲染完成
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  // 渲染后的影响
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flow-row">
+      <div className="md:w-56">
+        {/**Sidebar */}
+        <DashSidebar />
+      </div>
+      {/**profile */}
+      {tab == "profile" && <DashProfile />}
+    </div>
+  );
 }
