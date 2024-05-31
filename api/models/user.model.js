9fe9663746f453c9cb97bf865dd631e296fe37f7
Lovemysi <3236705586@qq.com>
2024-05-31 11:28:18 +0800

    complete profile page UI

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profilePicture: {
+      type: String,
+      default: "https://pic.starrylsi.top/img/2024/05/29/66572ba4ac621.jpg",
+    },
   },
   { timestamps: true }
 );
