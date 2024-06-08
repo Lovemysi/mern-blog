ffbc5330c69fca5a8d4e3ec0f5216b0d856d90cd
Lovemysi <3236705586@qq.com>
2024-06-08 13:40:38 +0800

    add admin fucntionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,7 +51,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(404, "User not found"));
     }
     // 3. 生成token
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     // 4. 将token发送给客户端
     const { password: pass, ...rest } = validUser._doc;
     res
